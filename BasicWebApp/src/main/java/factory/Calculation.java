f22776b1ae2fdf3aba089bb864525fd6c68ba8a9
rajeshchaky <110524222+rajeshchaky@users.noreply.github.com>
2023-08-03 23:13:50 +0530

    0308:Commit5, changed unit test due to corbetura failure

--- BasicWebApp/src/main/java/factory/Calculation.java
+++ BasicWebApp/src/main/java/factory/Calculation.java
@@ -2,30 +2,14 @@ package factory;
 
 public class Calculation {
 
-    public static int findMax1(int[] arr){
-        int max=0;
-        for(int i=1;i<arr.length;i++){
-            if(max<arr[i])
-                max=arr[i];
-        }
-        return max;
+
+    public static void test(){
+        System.out.println("testing");
     }
 
-    public static int findMax2(int[] arr){
-        int max=arr[0];//arr[0] instead of 0
-        for(int i=1;i<arr.length;i++){
-            if(max<arr[i])
-                max=arr[i];
-        }
-        return max;
-    }
-
-    public static boolean findMax3(int var1){
-        boolean flag=false;
-        int var2=3;
-        if(var1==var2){
-            flag=true;
-        }
-        return flag;
+    public static String returnString(){
+        System.out.println("This is the testcase in this class");
+        String str1="This is the testcase in this class";
+        return str1;
     }
 }
--- BasicWebApp/src/test/java/tests/RunTest.java
+++ BasicWebApp/src/test/java/tests/RunTest.java
@@ -7,24 +7,8 @@ import org.junit.jupiter.api.Test;
 public class RunTest {
 
 
-//    @Test
-//    public void testFindMax1(){
-//        int expectedValue=4;
-//        Assertions.assertEquals(expectedValue, Calculation.findMax1(new int[]{1,3,4,2}));
-//        //change the method in the below calculation.findmax2 method to findMax1 & you can re-create a junit failure*
-//        expectedValue=-1;
-//        Assertions.assertEquals(expectedValue, Calculation.findMax2(new int[]{-12,-1,-3,-4,-2}));
-//
-//    }
-//
-//    @Test
-//    public void testFindMax2(){
-//        Assertions.assertEquals(4, Calculation.findMax2(new int[]{1,3,4,2}));
-//        Assertions.assertEquals(-1, Calculation.findMax2(new int[]{-12,-1,-3,-4,-2}));
-//    }
-
     @Test
-    public void testFindMax3(){
-        Assertions.assertTrue(Calculation.findMax3(3));
+    public void test_JUnit() {
+        Assertions.assertEquals("This is the testcase in this class", Calculation.returnString());
     }
 }
